fd7fb93fe116d7cf2c940781b23193bd7b741aee
minervadeuteronomy <ccyowell@icloud.com>
2025-09-27 12:50:30 -0500

    Testing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,10 @@
 extends Sprite2D
+
+@export var time: int = 10
+
+func _process(delta: float)->void:
+	setTime(delta+time)
+func setTime(newTime: int)->void:
+	time = newTime
+func getTime()-> int:
+	return time;
--- scripts/ui_controller.gd
+++ scripts/ui_controller.gd
@@ -1,1 +1,6 @@
 extends Control
+@onready var playerScript = $"../Sprite2D"
+@export var timerLabel: Label
+
+func _process(delta: float) -> void:
+	timerLabel.text = str(playerScript.get_script().getTime())
